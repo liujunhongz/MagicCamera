23c44a4d821ff98f49b37e99c45507ecf6768fd3
liujunhong <liujunhong@lycam.tv>
2017-10-14 11:38:31 +0800

    .

--- Project-AndroidStudio/magicfilter/src/main/obj/local/armeabi/objs/MagicBeautify/bitmap/Conversion.o.d
+++
@@ -1,5 +0,0 @@
-/Users/su/Desktop/git/MagicCamera/Project-AndroidStudio/magicfilter/src/main/obj/local/armeabi/objs/MagicBeautify/bitmap/Conversion.o: \
-  /Users/su/Desktop/git/MagicCamera/Project-AndroidStudio/magicfilter/src/main/jni/bitmap/Conversion.cpp \
-  /Users/su/Desktop/git/MagicCamera/Project-AndroidStudio/magicfilter/src/main/jni/bitmap/Conversion.h
-
-/Users/su/Desktop/git/MagicCamera/Project-AndroidStudio/magicfilter/src/main/jni/bitmap/Conversion.h:
